041c8e806cbf3c7b773b48443ef7f5c9a75b1474
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-05-30 20:15:03 -0300

    Add pubSub module

---
+++ src/pubSub.js
@@ -0,0 +1,20 @@
+const events = {};
+
+function subscribe(eventName, f) {
+  if (!events[eventName]) events[eventName] = [];
+  events[eventName].push(f);
+}
+
+function publish(eventName, data) {
+  if (events[eventName]) {
+    events[eventName].forEach((f) => {
+      f(data);
+    });
+  }
+}
+
+function unsubscribe(eventName, f) {
+  if (events[eventName].includes(f)) events[eventName].splice(events[eventName].indexOf(f));
+}
+
+export default { subscribe, publish, unsubscribe };
