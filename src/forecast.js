13f788d0abf0fa85ce66c207ce8c8906e2c574f1
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-05-31 20:15:16 -0300

    Add overflow to forecast containers

--- src/forecast.js
+++ src/forecast.js
@@ -19,12 +19,24 @@ class Forecast {
     this.domNode = forecastElementFactory(name);
     this.dataArr = [...datum];
     this.cards = [];
+    this.numberOfCards = (() => {
+      switch (this.name) {
+        case 'Daily':
+          return 8;
+        case 'Hourly':
+          return 48;
+        case 'Minutely':
+          return 61;
+        default:
+          return 4;
+      }
+    })();
     pubSub.subscribe('newData', this.updateCards.bind(this));
     this.init();
   }
 
   init() {
-    for (let i = 0; i < 4; i += 1) {
+    for (let i = 0; i < this.numberOfCards; i += 1) {
       this.cards[i] = new Card('Title', this.name);
       for (let j = 0; j < this.dataArr.length; j += 1) {
         this.cards[i].addInfo(this.dataArr[j]);
@@ -34,8 +46,9 @@ class Forecast {
   }
 
   updateCards(data) {
+    const thisData = data[this.name.toLowerCase()];
     this.cards.forEach((card, index) => {
-      card.updateData(data[this.name.toLowerCase()][index], this.dataArr);
+      card.updateData(thisData[index], this.dataArr);
     });
   }
 }
--- src/mainSection.js
+++ src/mainSection.js
@@ -1,10 +1,14 @@
 import currentWeather from './currentWeather';
 import forecast from './forecast';
 
+const forecastContainer = document.createElement('div');
+forecastContainer.classList.add('forecast-container');
+
 const mainSection = (() => {
   const main = document.createElement('main');
   main.append(currentWeather);
-  main.append(forecast.daily.domNode, forecast.hourly.domNode);
+  main.append(forecastContainer);
+  forecastContainer.append(forecast.daily.domNode, forecast.hourly.domNode);
   return main;
 })();
 
