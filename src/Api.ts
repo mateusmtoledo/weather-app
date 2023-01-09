c161486c04aef688ba987f6e5cd4aa116ff79584
Mateus Toledo <mateusmtoledo1@gmail.com>
2023-01-08 23:30:38 -0300

    Add API response data types

--- src/Api.ts
+++ src/Api.ts
@@ -1,15 +1,21 @@
 import Loader from './components/Loader/Loader';
+import { RawWeatherData, WeatherAndLocationData } from './data';
 import WeatherUtils from './utils/WeatherUtils';
 
 const API_KEY = 'b8bedc2110030101dd9c8d0c74f2336c';
 
 // TODO error handling
 export default class Api {
-  static async getLocationData(locationName) {
+  static async getLocationData(locationName: string) {
     const response = await fetch(
       `https://api.openweathermap.org/geo/1.0/direct?q=${locationName}&limit=1&appid=${API_KEY}`
     );
-    const json = await response.json();
+    const json: {
+      name: string;
+      country: string;
+      lat: number;
+      lon: number;
+    }[] = await response.json();
     return {
       name: json[0].name,
       country: json[0].country,
@@ -18,22 +24,27 @@ export default class Api {
     };
   }
 
-  static async getWeatherByCoordinates(location) {
+  static async getWeatherByCoordinates(location: { lat: number; lon: number }) {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lon}&appid=${API_KEY}&units=metric`
     );
-    const json = await response.json();
+    const json: RawWeatherData = await response.json();
     return json;
   }
 
-  static async getWeatherByLocationName(locationName) {
+  static async getWeatherByLocationName(locationName: string) {
     const loader = new Loader();
     loader.show();
     const locationData = await Api.getLocationData(locationName);
     const weatherData = await Api.getWeatherByCoordinates(locationData);
     loader.remove();
-    weatherData.city = locationData.name;
-    weatherData.country = locationData.country;
-    return new WeatherUtils(weatherData).getNormalizedData();
+    const weatherAndLocationData: WeatherAndLocationData = {
+      ...weatherData,
+      city: locationData.name,
+      country: locationData.country,
+    };
+    weatherAndLocationData.city = locationData.name;
+    weatherAndLocationData.country = locationData.country;
+    return new WeatherUtils(weatherAndLocationData).getNormalizedData();
   }
 }
--- src/data.ts
+++ src/data.ts
@@ -75,3 +75,99 @@ export const visibility: DataType = {
   varName: 'visibility',
   unit: ' m',
 };
+
+export interface RawWeatherData {
+  lat: number;
+  lon: number;
+  timezone: string;
+  current: {
+    dt: number;
+    sunrise: number;
+    sunset: number;
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    uvi: number;
+    clouds: number;
+    visibility: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    weather: Array<WeatherCondition>;
+  };
+  minutely: {
+    dt: number;
+    precipitation: number;
+  }[];
+  hourly: {
+    dt: number;
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    uvi: number;
+    clouds: number;
+    visibility: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    pop: number;
+    weather: Array<WeatherCondition>;
+  }[];
+  daily: {
+    dt: number;
+    sunrise: number;
+    sunset: number;
+    moonrise: number;
+    moonset: number;
+    moon_phase: number;
+    temp: {
+      day: number;
+      min: number;
+      max: number;
+      night: number;
+      eve: number;
+      morn: number;
+    };
+    feels_like: {
+      day: number;
+      night: number;
+      eve: number;
+      morn: number;
+    };
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    weather: Array<WeatherCondition>;
+    clouds: number;
+    pop: number;
+    rain: number;
+    uvi: number;
+  }[];
+  alerts: {
+    sender_name: string;
+    event: string;
+    start: number;
+    end: number;
+    description: string;
+    tags: string[];
+  }[];
+}
+
+export interface WeatherAndLocationData extends RawWeatherData {
+  city: string;
+  country: string;
+}
+
+export interface WeatherCondition {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
